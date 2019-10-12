3a3bddcdb015c2d7b9db6466d445543e91e1aa8b
AdityaAthavale <Aditya.Athavale16@gmail.com>
2019-10-12 17:58:29 -0400

    First working version of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 class Character {
     name = ""
     healthPoints = 100
-    attackPower = 100
-    counterAttackPower = 100
+    baseAttackPower = 10
+    counterAttackPower = 10
+    currentAttackPower = 10
     image = ""
 
     constructor(name, health, attack, counterAttack, image) {
@@ -18,18 +19,20 @@ class Character {
     }
 }
 
-const minimumHealth = 100
-const minimumAttack = 150
-const minimumCounterAttack = 75
+const minimumHealth = 1
+const minimumAttack = 1
+const minimumCounterAttack = 1
 
 let characters = [];
 let usersPlayer = null;
+let computersPlayer = null;
+let isPickingUsersPlayer = true
 
 function createCharacters() {
-    const superHeros = ["Iron Man", "Super Man", "Spider Man", "Ant Man", "Thor", "Bat Man", "Hulk", "Wonder Girl", "Wasp", "Captain America", "Vision", "Black Panther", "She Hulk", "Hawkeye", "Falcon", "War Machine", "Scarlet Witch", "Nebula", "Gamora", "Rocket", "Groot", "Doctor Strange", "Wong", "Captain Marvel", "Thanos"]
+    const superHeros = ["Iron Man", "Super Man", "Spider Man", "Ant Man"]
     for (let i in superHeros) {
         let heroName = superHeros[i]
-        let char = new Character(heroName, (minimumHealth+(i*100)), (minimumAttack + (i * 50)), (minimumCounterAttack + (i * 40)), heroName)
+        let char = new Character(heroName, 100, minimumAttack + (i * minimumAttack), minimumCounterAttack + (i * minimumCounterAttack), ("./assets/images/" + heroName + ".jpeg"))   
         characters.push(char)
         $("#imageScroller").append(createImageTile(char, i))
     }
@@ -42,7 +45,7 @@ function createImageTile(char, index) {
     
     let firstRow = $('<div>')
     firstRow.addClass('row')
-    let imagePath = "./assets/images/" + "Iron Man" + ".jpeg"
+    let imagePath = char.imagePath
     let image = $('<img>')
     image.attr('src', imagePath)
     image.addClass('heroImage')
@@ -60,14 +63,44 @@ function createImageTile(char, index) {
     imageDiv.attr("id", index);
 
     imageDiv.click(function(event) {
-        usersPlayer = characters[this.id];
-        $('#' + this.id).css({'border' : '1px blue solid'})
+        if(isPickingUsersPlayer) {
+            usersPlayer = characters[this.id];
+            $('#usersPlayer').attr('src', "./assets/images/" + "Iron Man" + ".jpeg")
+            $('#characterPicker').text("Choose opponent player to continue:")
+            $('#userHealth').attr('aria-valuenow', 100)
+            $('#userHealth').attr('style','width:100%')
+            $('#userName').text(usersPlayer.name)
+            isPickingUsersPlayer = false
+        } else {
+            computersPlayer = characters[this.id]
+            $('#computersPlayer').attr('src', "./assets/images/" + "Iron Man" + ".jpeg")
+            $('#computerHealth').attr('aria-valuenow', 100)
+            $('#computerHealth').attr('style','width:100%')
+            $('#computerName').text(computersPlayer.name)
+            startGame()
+        }
     })
-
     return imageDiv
+}
+
+function startGame() {
+    $('#gameContainer').show()
+    $('#startUpContainer').hide()
 }
 
 $(document).ready(function() {
     createCharacters()
-    console.log(characters)
+    $('#startUpContainer').show()
+    $('#gameContainer').hide()
+
+    $('#attackButton').click(function() {
+        computersPlayer.healthPoints -= usersPlayer.attack;
+        usersPlayer.healthPoints -= computersPlayer.counterAttackPower
+
+        $('#computerHealth').attr('aria-valuenow', computersPlayer.healthPoints)
+        $('#computerHealth').attr('style','width:' + computersPlayer.healthPoints + '%')
+
+        $('#userHealth').attr('aria-valuenow', usersPlayer.healthPoints)
+        $('#userHealth').attr('style','width:'+usersPlayer.healthPoints+'%')
+    })
 })
